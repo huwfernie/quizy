935efc5e26f6833e7ed7f33c4112592d2b2f9fc9
Huw Fernie <HuwFernie@gmail.com>
2019-02-12 21:03:00 +0000

    initial front-end for dev work

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,29 @@ class App extends Component {
 render() {
     return (
       <div className="App">
-        <h3>Quizy</h3>
-        <p>still nothing here yet.</p>
+        <div className="hero-section">
+          <h1>Quizy</h1>
+        </div>
+        <div className="question-section">
+          <h2 className="question">Question text</h2>
+        </div>
+        <div className="submit-section">
+          <div className="button">Pick your answer</div>
+        </div>
+        <div className="options-section">
+          <div className="option">
+            option text
+          </div>
+          <div className="option">
+            option text
+          </div>
+          <div className="option">
+            option text
+          </div>
+          <div className="option">
+            option text
+          </div>
+        </div>
       </div>
     )
   }
