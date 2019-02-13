26e90372b74b0a4d50e2f0806f929d3f4e83705d
Huw Fernie <HuwFernie@gmail.com>
2019-02-13 20:07:56 +0000

    stylesheet update

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,16 @@ class App extends Component {
 render() {
     return (
       <div className="App">
-        <div className="hero-section">
+        <div className="section hero-section">
           <h1>Quizy</h1>
         </div>
-        <div className="question-section">
+        <div className="section question-section">
           <h2 className="question">Question text</h2>
         </div>
-        <div className="submit-section">
+        <div className="section submit-section">
           <div className="button">Pick your answer</div>
         </div>
-        <div className="options-section">
+        <div className="section options-section">
           <div className="option">
             option text
           </div>
